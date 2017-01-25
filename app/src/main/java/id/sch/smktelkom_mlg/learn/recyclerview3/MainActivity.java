b1a0d64278519aef2c4731008bc74a7c2cd41986
Wildan Ziaulhaq <wildan_ziaulhaq_24rpl@student.smktelkom-mlg.sch.id>
2017-01-25 08:31:28 +0700

    RecyclerView View Detail

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -1,15 +1,24 @@
 package id.sch.smktelkom_mlg.learn.recyclerview3;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ContextThemeWrapper;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import id.sch.smktelkom_mlg.learn.recyclerview3.adapter.HotelAdapter;
+
+public class MainActivity extends AppCompatActivity implements HotelAdapter.IHotelAdapter {
+
+    public static final String HOTEL = "hotel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,24 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void fillData() {
+        Resources resources = getResources();
+        String[] arJudul = resources.getStringArray(R.array.places);
+        String[] arDeskripsi = resources.getStringArray(R.array.place_desc);
+        String[] arDetail = resources.getStringArray(R.array.place_details);
+        String[] arLokasi = resources.getStringArray(R.array.place_locations);
+        TypedArray a = resources.obtainTypedArray(R.array.places_picture);
+        String[] arFoto = new String[a.length()];
+        for(int i=0;i < arFoto.length; i++) {
+            int id = a.getResourceId(i,0);
+            arFoto[i] = ContentResolver.SCHEME_ANDROID_RESOURCE+"://"
+                    +resources.getResourcePackageName(id)+'/'
+                    +resources.getResourceTypeName(id)+'/'
+                    +resources.getResourceEntryName(id);
+        }
+        a.recycle();
     }
 
     @Override
@@ -48,5 +75,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void doClick(int pos) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        Intent.putExtra(HOTEL, mList.get(pos));
+        startActivity(intent);
     }
 }
